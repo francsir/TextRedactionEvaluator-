5192b35ad540abc85767c2cb48e10ac53f7e0d47
francsir <72939196+francsir@users.noreply.github.com>
2024-03-26 17:33:03 +0000

    fixed wrod masking

--- medical_mask_fill.py
+++ medical_mask_fill.py
@@ -1,42 +1,71 @@
 from transformers import AutoTokenizer, AutoModel, pipeline
 import pandas as pd
-import concurrent.futures
 
 
-
+global j
+j = -1
 dataset = pd.read_csv('./medical_tc_test_masked_medical_3.csv')
-classifier = pipeline("fill-mask", model="medicalai/ClinicalBert")
-global i
-i = 0
-def process_chunk(chunk):
-    count = chunk.count('[MASK]')
-    for i in range(count):
-        temp = classifier(chunk)
-        try:
-            chunk = temp[0]['sequence']
-        except:
-            chunk = temp[0][0]['sequence']
-    return chunk
 
+tokenizer = AutoTokenizer.from_pretrained("medicalai/ClinicalBERT")
+#model = AutoModel.from_pretrained("medicalai/ClinicalBERT")
 
+classifier = pipeline("fill-mask", model="medicalai/ClinicalBert", tokenizer=tokenizer)
 
 def predict_sentence(text):
     ##get the first prediction
-    global i 
-    i = i+1
-    print(i)
+    trunc_text = []
+
     if len(text) > 512:
         chunks = [text[i:i + 512] for i in range(0, len(text), 512)]
     else:
         chunks = [text]
 
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        results = list(executor.map(process_chunk, chunks))
+    for chunk in chunks:
+        count = chunk.count('[MASK]')
+        
+        for i in range(count):
+            temp = classifier(chunk)
+            try:
+                chunk = temp[0]['sequence']
+            except:
+                chunk = temp[0][0]['sequence']
+            
+        trunc_text.append(chunk)
 
-    return ' '.join(results)
+    return ' '.join(trunc_text)
 
 
-dataset['predicted'] = dataset['masked'].apply(predict_sentence)
+def substitute_masked_words(sentence):
+    global j
+    j = j + 1
+    print(j)
+
+    mask_indices = [i for i, x in enumerate(sentence.split()) if x == "[MASK]"]
+    
+    predictions = classifier(sentence)
+
+    words = sentence.split()
+    mask_indices = [i for i, x in enumerate(words) if x == "[MASK]"]
+    
+    for i in range(len(mask_indices)):
+        words[mask_indices[i]] = predictions[i][0]['token_str']
+    return " ".join(words)
+    tokenized_sentence = tokenizer.tokenize(sentence)
+    for i, token in enumerate(tokenized_sentence):
+        if token == "[MASK]":
+            predictions = classifier(sentence)
+            try:
+                chunk = predictions[0]['token_str']
+            except:
+                chunk = predictions[0][0]['token_str']
+            predicted_word = chunk
+            tokenized_sentence[i] = predicted_word
+    return tokenizer.convert_tokens_to_string(tokenized_sentence)
+
+
+
+#print(substitute_masked_words(dataset['masked'].values[0]))
+dataset['predicted'] = dataset['masked'].apply(substitute_masked_words)
 
 
 dataset.to_csv(f'medicalData\Medical-Abstracts-TC-Corpus\medical_tc_test_masked_medical_4.csv', index=False)
