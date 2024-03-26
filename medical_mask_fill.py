59d8554ec565e6d4f11a64a07931252be50fb5da
francsir <72939196+francsir@users.noreply.github.com>
2024-03-26 11:14:29 +0000

    speeed up

--- medical_mask_fill.py
+++ medical_mask_fill.py
@@ -1,40 +1,42 @@
 from transformers import AutoTokenizer, AutoModel, pipeline
 import pandas as pd
+import concurrent.futures
 
 
 
 dataset = pd.read_csv('./medical_tc_test_masked_medical_3.csv')
-
-
-#tokenizer = AutoTokenizer.from_pretrained("medicalai/ClinicalBERT")
-#model = AutoModel.from_pretrained("medicalai/ClinicalBERT")
-
 classifier = pipeline("fill-mask", model="medicalai/ClinicalBert")
 
-def predict_sentence(text):
-    ##get the first prediction
-    trunc_text = []
+def process_chunk(chunk):
+    count = chunk.count('[MASK]')
+    print(1)
+    for i in range(count):
+        temp = classifier(chunk)
+        try:
+            chunk = temp[0]['sequence']
+        except:
+            chunk = temp[0][0]['sequence']
+    return chunk
 
+
+
+def predict_sentence(text, i):
+    ##get the first prediction
+    i = i+1
+    print(i)
     if len(text) > 512:
         chunks = [text[i:i + 512] for i in range(0, len(text), 512)]
     else:
         chunks = [text]
 
-    for chunk in chunks:
-        count = chunk.count('[MASK]')
-        for i in range(count):
-            temp = classifier(chunk)
-            try:
-                chunk = temp[0]['sequence']
-            except:
-                chunk = temp[0][0]['sequence']
-            
-        trunc_text.append(chunk)
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        results = list(executor.map(process_chunk, chunks))
 
-    return ' '.join(trunc_text)
+    return ' '.join(results)
 
 
-dataset['predicted'] = dataset['masked'].apply(predict_sentence)
+i = 0
+dataset['predicted'] = dataset['masked'].apply(predict_sentence(i))
 
 
 dataset.to_csv(f'medicalData\Medical-Abstracts-TC-Corpus\medical_tc_test_masked_medical_4.csv', index=False)
